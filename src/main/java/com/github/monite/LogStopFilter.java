4c02914d0e55034e734ad33d1e619e9aab600ed0
景天 <jingtian@qq.com>
2022-03-20 13:39:10 +0800

    首次提交

---
+++ src/main/java/com/github/monite/LogStopFilter.java
@@ -0,0 +1,23 @@
+package com.github.monite;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.filter.Filter;
+import ch.qos.logback.core.spi.FilterReply;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class LogStopFilter extends Filter<ILoggingEvent> {
+    private static FilterReply filterReply = FilterReply.ACCEPT;
+
+    @Override
+    public FilterReply decide(ILoggingEvent event) {
+        if (filterReply == FilterReply.DENY) {
+            return FilterReply.DENY;
+        }
+        if (event.getMessage().contains("JVM running")) {
+            filterReply = FilterReply.DENY;
+            log.info("应用已启动成功>>>>>>>>>>>>>>>>>>控制台日志已经停止输出!!!");
+        }
+        return FilterReply.ACCEPT;
+    }
+}
